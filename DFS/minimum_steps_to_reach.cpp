442edb624a2860c3be185f23dde67a0f81c0d1e6
Aditya Pandey <adityap2063@gmail.com>
2024-10-04 23:49:07 +0530

    add minimum_steps_to_reach

---
+++ DFS/minimum_steps_to_reach.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int steps(int source, int step, int dest)
+{
+	if (abs(source) > (dest)) 
+		return INT_MAX;
+	if (source == dest) return step;
+
+	int pos = steps(source + step + 1, 
+					step + 1, dest);
+
+	int neg = steps(source - step - 1,
+					step + 1, dest);
+
+	return min(pos, neg);
+}
+
+int main()
+{
+	int dest = 11;
+	cout << "No. of steps required to reach "
+							<< dest << " is "
+						<< steps(0, 0, dest);
+	return 0;
+}
